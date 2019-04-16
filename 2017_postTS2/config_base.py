ef84e365d46da30d110d519a58dcf7313927acde
Jan Kašpar <jan.kaspar@cern.ch>
2019-04-16 18:41:27 +0200

    New alignment, cuts adjusted.

--- 2017_postTS2/config_base.py
+++ 2017_postTS2/config_base.py
@@ -5,7 +5,8 @@ config = cms.PSet(
     xangle = cms.uint32(0),
     dataset = cms.string(""),
 
-    alignment_files = cms.vstring("/afs/cern.ch/work/j/jkaspar/software/ctpps/development/ctpps_initial_proton_reconstruction_CMSSW_10_2_0/CMSSW_10_2_0/src/RecoCTPPS/ProtonReconstruction/data/alignment/2017_postTS2/collect_alignments_2018_10_25.5.out"),
+    #alignment_files = cms.vstring("/afs/cern.ch/work/j/jkaspar/software/ctpps/development/ctpps_initial_proton_reconstruction_CMSSW_10_2_0/CMSSW_10_2_0/src/RecoCTPPS/ProtonReconstruction/data/alignment/2017_postTS2/collect_alignments_2018_10_25.5.out"),
+    alignment_files = cms.vstring("/afs/cern.ch/work/j/jkaspar/software/ctpps/development/ctpps_initial_proton_reconstruction_CMSSW_10_2_0/CMSSW_10_2_0/src/RecoCTPPS/ProtonReconstruction/data/alignment/2017_postTS2/fit_alignments_2019_04_15.1.out"),
 
     alignment_corrections = cms.PSet(
       rp_L_2_F = cms.PSet(
@@ -34,19 +35,19 @@ config = cms.PSet(
 
 	  cut_v_apply = cms.bool(True),
 	  cut_v_a = cms.double(-1.07),
-	  cut_v_c = cms.double(+1.63),
+	  cut_v_c = cms.double(0.),
 	  cut_v_si = cms.double(0.15)
     ),
 
     sector_56 = cms.PSet(
 	  cut_h_apply = cms.bool(True),
 	  cut_h_a = cms.double(-1),
-	  cut_h_c = cms.double(0),
+	  cut_h_c = cms.double(+0.2),
 	  cut_h_si = cms.double(0.2),
 
 	  cut_v_apply = cms.bool(True),
 	  cut_v_a = cms.double(-1.07),
-	  cut_v_c = cms.double(+1.3),
+	  cut_v_c = cms.double(0.),
 	  cut_v_si = cms.double(0.15)
     ),
 
